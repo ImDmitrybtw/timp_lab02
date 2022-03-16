904376e36924b749f0ed004a70b96f249a8f4ea8
ImDmitrybtw <92674699+ImDmitrybtw@users.noreply.github.com>
2022-03-17 02:05:24 +0300

    Rebasing branch 'patch2' on main

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main(){
-   std::cout << "Enter your name"; //Requesting a username
-   std::string name;
-   std::cin >> name;//Remembering a username
-   std::cout << "Hello world from " << name;//Output 
-   return 0;
+int
+main()
+{
+  std::cout << "Enter your name"; //Requesting a username
+  std::string name;
+  std::cin >> name; //Remembering a username
+  std::cout << "Hello world from " << name; //Output
+  return 0;
 }
